f125b1e64d97b7acb7a492a605b41c7a4402a281
mamun-prodhan <mamunpr9@gmail.com>
2023-04-01 10:01:12 +0600

    js practice problem

---
+++ pp1.js
@@ -0,0 +1,14 @@
+// var moneyGiven = 1000;
+// var totalCost = 700;
+// var moneyReturned = moneyGiven - totalCost;
+// console.log(moneyReturned);
+
+// var firstLine = " I am going to be";
+// var secondLine = "an awesome web developer";
+
+// var combinedLine = firstLine + " " + secondLine;
+// console.log(combinedLine);
+
+// var number = 119;
+// var reminder = 199 % 5;
+// console.log(reminder);
