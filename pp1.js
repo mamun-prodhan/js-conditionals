81ed75463e07987980b0b2348247e911c65932a5
mamun-prodhan <mamunpr9@gmail.com>
2023-04-01 10:09:33 +0600

    pp

--- pp1.js
+++ pp1.js
@@ -12,3 +12,18 @@
 // var number = 119;
 // var reminder = 199 % 5;
 // console.log(reminder);
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+for(var i = 0; i < fruits.length; i++){
+    
+    if(fruits[i] === 'Banana'){
+        console.log('index of banana: ', i);
+        fruits[i] = 'Mango';
+    }
+    
+}
+
+fruits[2] = 'Watermelon';
+
+console.log(fruits);
