ec3ef39b9d52e1f55b30e2e301f5566285afd913
mamun-prodhan <mamunpr9@gmail.com>
2023-04-11 10:32:41 +0600

    sum of all odd numbers

--- sumOfAllNumbers.js
+++ sumOfAllNumbers.js
@@ -1,4 +1,4 @@
-function sumOfAllNumbers(array){
+function sumOfOddNumbers(array){
     var sum = 0;
     for(var i = 0 ; i < array.length; i++){
         sum = sum + array[i];
@@ -6,5 +6,22 @@ function sumOfAllNumbers(array){
     return sum;
 }
 
-var result = sumOfAllNumbers([10,20,30,40,50]);
-console.log(result);
+// var result = sumOfAllNumbers([10,20,30,40,50,7,3]);
+// console.log(result);
+
+function getOddNumbersOfAnArray(numbers){
+    const oddNumbers = [];
+    for(let i = 0; i < numbers.length; i++){
+        if( numbers[i] % 2 !== 0)
+        {
+            console.log(numbers[i]);
+            oddNumbers.push(numbers[i]);
+        }
+    }
+    return oddNumbers;
+}
+
+const oddNumbers = (getOddNumbersOfAnArray([1,2,5,10]))
+
+const oddNumberSum = sumOfOddNumbers(oddNumbers);
+console.log(oddNumberSum);
