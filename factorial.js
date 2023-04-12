196a40ecbf2af915b6ac2fd55209de9f2bdd9945
mamun-prodhan <mamunpr9@gmail.com>
2023-04-12 10:00:09 +0600

    practice

--- factorial.js
+++ factorial.js
@@ -1,23 +1,23 @@
-// function factorial(number){
-//     let mul = 1;
-//     for(let i = 1; i <=number; i++){
-//         mul = mul * i;
-//     }
-//     return mul;
-// }
-
-// const fact = factorial(10);
-// console.log(fact);
-
 function factorial(number){
-    let result = 1;
-    let i = number;
-    while(i <= number){
-        result = result * i;
-        i--;
+    let mul = 1;
+    for(let i = 1; i <=number; i++){
+        mul = mul * i;
     }
-    return result;
+    return mul;
 }
 
-const fact = factorial(3);
+const fact = factorial(10);
 console.log(fact);
+
+// function factorial(number){
+//     let result = 1;
+//     let i = 4;
+//     while(i <= number){
+//         result = result * i;
+//         i--;
+//     }
+//     return result;
+// }
+
+// const results = factorial(4);
+// console.log(results);
