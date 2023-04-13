3ab9a83079e43725d0accf14286894aff6cb2caa
mamun-prodhan <mamunpr9@gmail.com>
2023-04-13 11:40:54 +0600

    conditional max number

---
+++ max.js
@@ -0,0 +1,14 @@
+function findTheMax(num1, num2, num3){
+    if(num1 > num2 && num1 > num3){
+        return num1;
+    }
+    else if(num2 > num1 && num2 > num3){
+        return num2;
+    }
+    else{
+        return num3;
+    }
+}
+
+const result = findTheMax(32, 45, 12);
+console.log(result);
