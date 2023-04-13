3016e425b0f75ae0b5acf561df390a3fc284eaa0
mamun-prodhan <mamunpr9@gmail.com>
2023-04-13 11:36:42 +0600

    maximum number

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 const lyrics = 'Tumi bondhu kala pakhi ami jeno ki, bosonto kale tomay bolte pari ni';
 
-const doesExist = lyrics.includes('pakhi');
+const lyricSplit = lyrics.split(',');
 
-console.log(doesExist);
+console.log(lyricSplit);
