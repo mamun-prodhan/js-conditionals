2d4e727e4afb01a7fa4172eaecf82e2a7548f93c
mamun-prodhan <mamunpr9@gmail.com>
2023-04-16 12:14:25 +0600

    product quantity

--- sumOfShoppingCart.js
+++ sumOfShoppingCart.js
@@ -1,20 +1,21 @@
 const shoppingCart = [
-    {name: 'shoe', price: 1500},
-    {name: 'pant', price: 1000},
-    {name: 'shirt', price: 500},
-    {name: 'watch', price: 1000},
-    {name: 'panjabi', price: 1000}
+    {name: 'shoe', price: 1500, quantity: 2},
+    {name: 'pant', price: 1000, quantity: 2},
+    {name: 'shirt', price: 500, quantity: 4},
+    {name: 'watch', price: 1000, quantity: 3},
+    {name: 'panjabi', price: 100, quantity: 5}
 ]
 
 function totalCost(shoppingCart){
     let sum = 0;
     for(let i = 0; i < shoppingCart.length; i++){
         const product = shoppingCart[i]
-        sum = sum + product.price;
+        const productTotal = product.price * product.quantity;
+        sum = sum + productTotal;
     }
     
     return sum;
 }
 
 const result = totalCost(shoppingCart);
-console.log(result);
+console.log(result)
